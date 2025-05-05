2e5259fd5e4a702e7e9c01b7987002c8b0043ef7
AK <aalkhareji27@gmail.com>
2025-05-05 18:44:42 +0300

    Refactor global styles and layout for improved theming; update CSS variables for light and dark modes, integrate Cairo font, and create protected layout with a navbar. Add dashboard page and enhance mode toggle functionality with client-side rendering.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 const ModeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <Button variant="secondary" size="icon">
+        <MoonIcon />
+      </Button>
+    );
+  }
 
   return (
     <Button
